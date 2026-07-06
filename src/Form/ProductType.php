caeabcd0b3a27daac3fc0d925d8a6a69008687c8
Damir <damir.brera.eb@gmail.com>
2026-07-07 00:56:00 +0200

    Add PHPStan static analysis at level 6
    Adds PHPStan with the Symfony and Doctrine extensions, analysing src at level 6
    with no errors, and runs it in CI. Documents the form data types with @extends
    generics so the analysis passes.

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -17,6 +17,9 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+/**
+ * @extends AbstractType<ProductFormData>
+ */
 class ProductType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
